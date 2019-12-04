f62130414d01e80554140ee86e41c814bbca26d2
hrakotomanana <hrakotomanana@PC-EXL-RUN-10.mpi.grp>
2019-12-04 17:39:42 +0300

    Adding Auth service

--- src/main/java/com/example/webapi/config/SecurityConfig.java
+++ src/main/java/com/example/webapi/config/SecurityConfig.java
@@ -1,9 +1,24 @@
 package com.example.webapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.webapi.security.JwtAuthenticationEntryPoint;
+import com.example.webapi.security.JwtAuthenticationFilter;
+import com.example.webapi.service.UserAuthService;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +27,60 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 		jsr250Enabled = true,
 		prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private JwtAuthenticationEntryPoint unAuthorizeHandler;
+	
+	@Autowired
+	private UserAuthService userAuthService;
+	
+	@Bean
+	public JwtAuthenticationFilter filter() {
+		return new JwtAuthenticationFilter();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean(BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userAuthService).passwordEncoder(passwordEncoder());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors()
+				.and()
+			.csrf()
+				.disable()
+			.exceptionHandling()
+				.authenticationEntryPoint(unAuthorizeHandler)
+				.and()
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+			.authorizeRequests()
+				.antMatchers(
+						"/",
+						"/**/*.html",
+						"/**/*.js",
+						"/**/*.css").permitAll()
+				.antMatchers("/api/auth/**").permitAll()
+				.antMatchers(HttpMethod.GET, "/api/forfaits").permitAll()
+				.anyRequest().authenticated();
+		
+		http.addFilterBefore(filter(), UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	
 
 }
---
+++ src/main/java/com/example/webapi/service/UserAuthService.java
@@ -0,0 +1,27 @@
+package com.example.webapi.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.webapi.exceptions.RessourceNotFoundException;
+import com.example.webapi.model.User;
+import com.example.webapi.repository.UserRepository;
+
+public class UserAuthService implements UserDetailsService {
+	
+	@Autowired
+	UserRepository repo;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repo.findUserByUsername(username).orElseThrow(
+				() -> new RessourceNotFoundException("User", "id", 0));
+		return user;
+	}
+
+}
