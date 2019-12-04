364d5c4a805581a04a599e01212438c04d1a9846
hrakotomanana <hrakotomanana@PC-EXL-RUN-10.mpi.grp>
2019-12-04 14:13:42 +0300

    update config

--- src/main/java/com/example/webapi/ExelciaWebapiEclApplication.java
+++ src/main/java/com/example/webapi/ExelciaWebapiEclApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.annotations.SwaggerDefinition;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableSwagger2
+@SwaggerDefinition(basePath = "/api")
 public class ExelciaWebapiEclApplication {
 
 	public static void main(String[] args) {
