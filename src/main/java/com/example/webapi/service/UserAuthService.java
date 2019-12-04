90592d21082751b26eed8456459d793def3f1811
hrakotomanana <hrakotomanana@PC-EXL-RUN-10.mpi.grp>
2019-12-04 17:58:07 +0300

    fix service

--- src/main/java/com/example/webapi/service/UserAuthService.java
+++ src/main/java/com/example/webapi/service/UserAuthService.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.example.webapi.exceptions.RessourceNotFoundException;
 import com.example.webapi.model.User;
 import com.example.webapi.repository.UserRepository;
 
+@Service
 public class UserAuthService implements UserDetailsService {
 	
 	@Autowired
