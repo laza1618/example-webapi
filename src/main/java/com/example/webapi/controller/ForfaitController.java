ba5f15bd1def5d03a48406113f5f5e24b64ac1ed
hrakotomanana <hrakotomanana@PC-EXL-RUN-10.mpi.grp>
2019-12-04 04:01:18 -0400

    update controller

--- src/main/java/com/example/webapi/controller/ForfaitController.java
+++ src/main/java/com/example/webapi/controller/ForfaitController.java
@@ -2,9 +2,15 @@ package com.example.webapi.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +37,19 @@ public class ForfaitController {
 		return repo.findById(forfaitId).orElseThrow(
 				() -> new RessourceNotFoundException("Forfait", "id", forfaitId)
 		);
+	}
+	
+	@PostMapping("/forfaits")
+	public Forfait createForfait(@Valid @RequestBody Forfait forfait) {
+		return repo.save(forfait);
+	}
+	
+	@DeleteMapping("/forfaits/{id}")
+	public ResponseEntity<Forfait> deleteForfaitById(@PathVariable(value="id")Long forfaitId) {		
+		Forfait forfait = repo.findById(forfaitId).orElseThrow(
+				() -> new RessourceNotFoundException("Forfait", "id", forfaitId));
+		repo.delete(forfait);
+		return ResponseEntity.ok(forfait);
 	}
 	
 
