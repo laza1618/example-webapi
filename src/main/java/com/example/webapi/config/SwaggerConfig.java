21b3aae050deff1fa239efbfc88211de8775d4c7
hrakotomanana <hrakotomanana@PC-EXL-RUN-10.mpi.grp>
2019-12-04 14:01:47 +0300

    swagger conf

---
+++ src/main/java/com/example/webapi/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.example.webapi.config;
+
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+            .apis(RequestHandlerSelectors
+                .basePackage("com.exelcia.webapi.controller"))
+            .paths(PathSelectors.regex("/.*"))
+            .build().apiInfo(apiEndPointsInfo());
+    }
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+            .description("Formation Management REST API")
+            .contact(new Contact("","",""))
+            .license("Apache 2.0")
+            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+            .version("1.0.0")
+            .build();
+    }	
+
+}
